ed318b0d8ca082a4fb2e1981fe5eb0a3c06e166c
MQJ_yo <57799386+jieloveyo@users.noreply.github.com>
2022-11-26 22:27:19 +0800

    svm-finish

--- assignment/assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment/assignment1/cs231n/classifiers/linear_classifier.py
@@ -66,7 +66,10 @@ class LinearClassifier(object):
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-            pass
+            #mask = np.random.choice(range(num_train), batch_size, replace=True) #replace 表示是有取出来的数有重复
+            mask = np.random.choice(range(num_train), batch_size)
+            X_batch = X[mask]
+            y_batch = y[mask]
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -81,7 +84,7 @@ class LinearClassifier(object):
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-            pass
+            self.W -= learning_rate * grad
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -111,7 +114,7 @@ class LinearClassifier(object):
         ###########################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        y_pred = np.argmax(X.dot(self.W), axis=1)
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return y_pred
--- assignment/assignment1/cs231n/classifiers/linear_svm.py
+++ assignment/assignment1/cs231n/classifiers/linear_svm.py
@@ -27,22 +27,38 @@ def svm_loss_naive(W, X, y, reg):
     # compute the loss and the gradient
     num_classes = W.shape[1]
     num_train = X.shape[0]
-    loss = 0.0
+    loss = 0.0  #损失函数
     for i in range(num_train):
-        scores = X[i].dot(W)
-        correct_class_score = scores[y[i]]
+        scores = X[i].dot(W) #(1,D) * (D,C) --> (1,C)
+        correct_class_score = scores[y[i]]  #标签类的得分
         for j in range(num_classes):
-            if j == y[i]:
+            if j == y[i]:   #跳过标签类
                 continue
             margin = scores[j] - correct_class_score + 1  # note delta = 1
             if margin > 0:
                 loss += margin
+                #########################################################
+                #                    START OF CHANGE                    #
+                #########################################################
+                ds = 1 / num_train    #非标签类
+                dys = -1 / num_train    #标签类
+
+                ds_dW = X[i]    #ds/dW
+                dW[:,j] += ds * ds_dW    #W:(D,C),第j列表示第j类的权重参数
+                dW[:,y[i]] += dys * ds_dW
+
+                # 化简后的写法
+                #dW[:, j] += X[i]    # update gradient for incorrect label
+                #dW[:, y[i]] -= X[i] # update gradient for correct label
+                #########################################################
+                #                     END OF CHANGE                     #
+                #########################################################
 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
     loss /= num_train
 
-    # Add regularization to the loss.
+    # Add L2 regularization to the loss.
     loss += reg * np.sum(W * W)
 
     #############################################################################
@@ -55,7 +71,8 @@ def svm_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    # dW /= num_train   # scale gradient ovr the number of samples(done before)
+    dW += 2 * reg * W # append partial derivative of regularization term(for l2 regularization)
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -77,8 +94,17 @@ def svm_loss_vectorized(W, X, y, reg):
     # result in loss.                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    # X.shape = (N,D) = (N,3073)
+    # W.shape = (D,C) = (3073,10)
+    N = X.shape[0]
 
-    pass
+    scores = X.dot(W)   #(N,D) * (D,C) -> (N,C)
+    scores_label = scores[range(N),[y]].T #(1,N)->(N,1)，此处转置是为了下面的广播
+
+    margins = np.maximum(0, scores - scores_label + 1) #(N,C)
+    margins[range(scores.shape[0]), y] = 0;    #标签类不进行损失计算
+
+    loss = np.sum(margins) / N + reg*np.sum(W**2)  #(N,C)
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -92,9 +118,23 @@ def svm_loss_vectorized(W, X, y, reg):
     # loss.                                                                     #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    
+    # 由链式法则进行一步一步的回推
+    # dloss/dW = dloss/dmargins * dmargins/dscores * dscores/dW
+    
+    # 构造dloss_tmp,dscores
+    dmargins = np.ones_like(margins) / N  #(N,C)
+    dscores = np.zeros_like(scores) #(N,C)
 
-    pass
+    dscores[margins>0] = 1 #margins = np.maximum(0, scores - scores_label + 1)
+    dscores[range(N), y] = -np.sum(dscores, axis=1) #
+    dscores *= dmargins    # *(1/N)
 
+    # 具体推导暂未看懂
+    # dW(D,C) X(N,D) dscores(N,C)  
+    dW = X.T.dot(dscores)
+
+    dW += 2 * reg * W #正则化参数梯度
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     return loss, dW
